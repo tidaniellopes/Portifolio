1b26c4602c8ab643bd96d5d6f753430c2da3f0e0
tidaniel.lopes <tidaniellopes@gmail.com>
2020-09-03 23:42:12 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,39 @@
 $(function(){
 
-    //Variáveis paineis
 
-    var sobre,projetos,tecnologias,contato;
-
-    sobre = $('#sobre');
-    projetos = $('#projetos');
-    tecnologias = $('#tecnologias');
-    contato = $('#contato');
-   
-
-    $(sobre).click(function(){
-        $('.sobre').css('display','block');
-    })
+    clickNav();
     
+    function clickNav(){
+        
+        $('#sobre').click(function(){
+            $('.nav').css('display','none');
+            $('.sobre').css('display','block')
+
+        })
+
+        $('#projetos').click(function(){
+            $('.nav').css('display','none');
+            $('.projetos').css('display','block')
+
+        })
+
+        $('#tecnologias').click(function(){
+            $('.nav').css('display','none');
+            $('.tecnologias').css('display','block')
+
+        })
+
+        $('#contato').click(function(){
+            $('.nav').css('display','none');
+            $('.contato').css('display','block')
+
+        })
+
+    }
+
+
+    
+
+
+
 })
