38b9512b082861eaade4d42f7d608ba6a7bbd38c
tidaniel.lopes <tidaniellopes@gmail.com>
2020-09-03 22:04:47 -0300

    update

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    //Variáveis paineis
+
+    var sobre,projetos,tecnologias,contato;
+
+    sobre = $('#sobre');
+    projetos = $('#projetos');
+    tecnologias = $('#tecnologias');
+    contato = $('#contato');
+   
+
+    $(sobre).click(function(){
+        $('.sobre').css('display','block');
+    })
+    
+})
