8352b9e8787fcd2ef38b2291b968954c00c04bd4
tidaniel.lopes <tidaniellopes@gmail.com>
2020-09-05 00:30:26 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,57 @@
 $(function(){
 
-
+    //Clique menu
     clickNav();
+    clickMenu();
     
     function clickNav(){
         
         $('#sobre').click(function(){
             $('.nav').css('display','none');
-            $('.sobre').css('display','block')
-
+            $('.sobre').css('display','block');
+            
         })
 
         $('#projetos').click(function(){
             $('.nav').css('display','none');
-            $('.projetos').css('display','block')
-
+            $('.projetos').css('display','block');
+            $('.wraper').css('transition','0.8s');
+            $('.wraper').css('opacity','1');
+            
         })
 
         $('#tecnologias').click(function(){
             $('.nav').css('display','none');
-            $('.tecnologias').css('display','block')
+            $('.tecnologias').css('display','block');
+            
 
         })
 
         $('#contato').click(function(){
             $('.nav').css('display','none');
-            $('.contato').css('display','block')
+            $('.contato').css('display','block');
+            
 
         })
+
+    }
+
+
+    function clickMenu(){
+
+        var clickCheck = false;
+
+        if(clickCheck == false){
+
+            $('.menu-mobile').click(function(){
+                $('.menu').slideToggle();
+
+
+            })
+            
+        }
+
+
 
     }
 
